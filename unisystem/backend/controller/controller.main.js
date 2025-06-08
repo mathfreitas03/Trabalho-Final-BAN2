2c647141ade18b7824476662c3670634f4f79c7c
mathfreitas03 <116849424+mathfreitas03@users.noreply.github.com>
2025-06-08 00:21:44 -0300

    add most of the controller functions and generated tuples to populate the database

--- unisystem/backend/controller/controller.main.js
+++ unisystem/backend/controller/controller.main.js
@@ -1,5 +1,139 @@
-const searchRegisterButton = document.querySelector('.search-register-button')
+const searchRegistryButton = document.querySelector('.search-register-button')
+const form = document.querySelector('.register-form')
+const registryOption = form.querySelector('#tipo-registro')
 
-searchRegisterButton.addEventListener('click', () => {
-    alert('TESTE')
-})
+function loadForm(option) {
+    const registerForm = document.querySelector('.register-form');
+    const todasFormRows = registerForm.querySelectorAll('.form-row');
+
+    todasFormRows.forEach(div => {
+        if (!div.querySelector('#tipo-registro')) {
+            div.remove();
+        }
+    });
+
+    let novosCampos = '';
+
+    switch(option) {
+        case 'projeto':
+            novosCampos = `
+                <div class="form-row">
+                    <label for="nome-projeto">Nome do projeto</label>
+                    <input type="text" name="nome-projeto" id="nome-projeto">
+                </div>
+                <div class="form-row">
+                    <label for="professor-responsavel">Professor responsável</label>
+                    <input type="text" name="professor-responsavel" id="professor-responsavel">
+                </div>
+                <div class="form-row">
+                    <label for="matricula-responsavel">Matrícula responsável</label>
+                    <input type="text" name="matricula-responsavel" id="matricula-responsavel">
+                </div>
+                <div class="form-row">
+                    <label for="orgao-financiador">Órgão financiador</label>
+                    <input type="text" name="orgao-financiador" id="orgao-financiador">
+                </div>
+                <div class="form-row">
+                    <label for="valor-minimo">Orçamento mínimo</label>
+                    <input type="number" min=0 name="valor-minimo" id="valor-minimo">
+                </div>
+                <div class="form-row">
+                    <label for="valor-maximo">Orçamento máximo</label>
+                    <input type="number" min=0 name="valor-maximo" id="valor-maximo">
+                </div>
+                `
+            break;
+        case 'professor':
+            novosCampos = `
+                <div class="form-row">
+                    <label for="nome-professor">Nome</label>
+                    <input type="text" name="nome-professor" id="nome-professor">
+                    </div>
+                <div class="form-row">
+                    <label for="matricula">Mátricula</label>
+                    <input type="text" name="matricula" id="matricula"></input>
+                </div>
+                <div class="form-row">
+                    <label for="especialidade-professor">Especialidade</label>
+                    <input type="text" name="especialidade-professor" id="especialidade-professor">
+                </div>
+                <div class="form-row">
+                    <label for="idade-professor">Idade</label>
+                    <input type="text" name="idade-professor" id="idade-professor">
+                </div>
+                <div class="form-row">
+                    <label for="sala-professor">Sala</label>
+                    <input type="text" name="sala-professor" id="sala-professor">
+                </div>
+                `
+            break;
+        case 'departamento':
+                novosCampos = `<div class="form-row">
+                    <label for="nome-departamento">Nome do departamento</label>
+                    <input type="text" name="nome-departamento" id="nome-departamento">
+                </div>
+                <div class="form-row">
+                    <label for="numero-departamento">Número do departamento</label>
+                    <input type="number" name="numero-departamento" id="numero-departamento"></input>
+                </div>
+                <div class="form-row">
+                    <label for="endereco-escritorio">Endereço</label>
+                    <input type="text" name="endereco-escritorio" id="endereco-escritorio">
+                </div>
+                <div class="form-row">
+                    <label for="professor-responsavel">Professor Responsável</label>
+                    <input type="text" name="professor-responsavel" id="professor-responsavel">
+                </div>
+                <div class="form-row">
+                    <label for="matricula-professor-responsavel">Mátricula Responsável</label>
+                    <input type="text" name="matricula-professor-responsavel" id="matricula-professor-responsavel">
+                </div>`
+        break;
+        case 'orgao-financiador':
+            case 'departamento':
+                novosCampos = `<div class="form-row">
+                    <label for="nome-orgao">Nome</label>
+                    <input type="text" name="nome-orgao" id="nome-orgao">
+                </div>
+                <div class="form-row">
+                    <label for="numero-orgao">Código do orgão</label>
+                    <input type="number" name="numero-orgao" id="numero-orgao"></input>
+                </div>`
+        break;
+        case 'estudante':
+            novosCampos = `
+                <div class="form-row">
+                    <label for="nome-estudante">Nome</label>
+                    <input type="text" name="nome-estudante" id="nome-estudante">
+                </div>
+                <div class="form-row">
+                    <label for="idade-estudante">Idade</label>
+                    <input type="text" name="idade-estudante" id="idade-estudante">
+                </div>
+                <div class="form-row">
+                    <label for="matricula">Mátricula</label>
+                    <input type="text" name="matricula" id="matricula"></input>
+                </div>
+                <div class="form-row">
+                    <label for="matricula-supervisor">Mátricula Supervisor</label>
+                    <input type="text" name="matricula-supervisor" id="matricula-supervisor"></input>
+                </div>
+                <div class="form-row">
+                    <label for="tipo-curso">Grau cursando</label>
+                    <input type="text" name="tipo-curso" id="tipo-curso"></input>
+                </div>
+                <div class="form-row">
+                    <label for="departamento-estudante">Departamento</label>
+                    <input type="text" name="departamento-estudante" id="departamento-estudante"></input>
+                </div>
+                `
+        break;
+    }
+
+    registerForm.insertAdjacentHTML('beforeend', novosCampos);
+}
+
+registryOption.addEventListener('change', () => {
+    loadForm(registryOption.value) 
+    }
+)
--- unisystem/backend/server.js
+++ unisystem/backend/server.js
@@ -3,6 +3,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
 import userRoute from './routes/user.routes.js';
+import fs from 'fs'
+import pg from 'pg'
 
 // Estas duas linhas são necessárias para pegar __dirname em ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +12,20 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config()
+
+const config = {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('ca.pem').toString(),
+    },
+};
+
+const pool = new pg.Pool(config);
 
 const PORT = process.env.PORT || 3000
 
@@ -31,3 +47,23 @@ app.get('/', (req, res) => {
         }
     });
 })
+
+async function testeCon() {
+  try {
+    await pool.connect();
+    const resultado = await pool.query('select * from professor');
+    return resultado.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.get('/testar_conexao', async (req, res) => {
+  try {
+    const consulta = await testeCon();
+    res.send(consulta);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro na conexão');
+  }
+});
